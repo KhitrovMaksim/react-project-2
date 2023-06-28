7697a2d7c69a7a2db65097047f7fc5231c7590df
Maksim <Khitrov.Maks@gmail.com>
2023-06-28 23:21:27 +0400

    Added simple react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [likes, setLikes] = useState(0);
+    const [value, setValue] = useState('Text in input')
+
+    function increment() {
+        setLikes(likes + 1)
+    }
+
+    function decrement() {
+        setLikes(likes - 1)
+    }
+
+    return (
+        <div className="App">
+            <h1>{value}</h1>
+            <input
+                type="text"
+                value={value}
+                onChange={event => setValue(event.target.value)}
+            />
+            <hr/>
+            <h1>{likes}</h1>
+            <button onClick={decrement}>Decrement -</button>
+            <button onClick={increment}>Increment +</button>
+        </div>
+    );
 }
 
 export default App;
